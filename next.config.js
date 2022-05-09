e46061f5ee18efb2d2f7600515c4694ea2df675c
Max Pod <github@maxpod.me>
2022-05-09 13:29:37 -0400

    Failing because _vfile.VFile is not a constructor

--- next.config.js
+++ next.config.js
@@ -2,6 +2,6 @@ const withTM = require('next-transpile-modules')(['@mdx-js/mdx','next-mdx-remote
 
 module.exports = withTM({
   experimental: {
-    // esmExternals: 'loose'
+    esmExternals: 'loose'
   }
 });
